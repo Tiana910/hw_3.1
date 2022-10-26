733585a76c870a5abe34560f6265884e168c36f5
Tiana910 <tatsiana.magonova@gmail.com>
2022-10-26 15:59:21 +0300

    test for App

--- src/components/MessageList/MessageList.test.jsx
+++ src/components/MessageList/MessageList.test.jsx
@@ -1,17 +1,32 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {  render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
-import {MessageList} from './MessageList'
+import { MessageList } from './MessageList'
+import { AUTHOR } from "../constants";
 
 
-describe('MessageList', ()=>{
-    it('render component', ()=> {
-        render(<MessageList messages={[]}/>);
+describe('MessageList', () => {
+    it('render component', () => {
+        render(<MessageList messages={[]} />);
     });
 
-    it("MessageList is empty", ()=>{
-        render(<MessageList messages={[]}/>);
+    it("MessageList is empty", () => {
+        render(<MessageList messages={[]} />);
         expect(screen.queryAllByRole('li').length).toBe(0);
     });
-    
+
+    it("MessageList length is 2", () => {
+        const messages = [
+            {
+                autor: AUTHOR.user,
+                value: 'first'
+            },
+            {
+                autor: AUTHOR.user,
+                value: 'second'
+            }
+        ]
+        render(<MessageList messages={[messages]} />);
+        expect(screen.getAllByTestId('li').length).toBe(2);
+        expect(screen.getAllByTestId('li')[0].innerHTML).toBe("user:first");
+    });
 });
